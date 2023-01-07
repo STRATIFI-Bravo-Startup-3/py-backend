c3dbad8ee790084ed06a9b5aa1e020072e820a61
Agadaga Joseph <josephagadaga@gmail.com>
2023-01-07 20:50:33 +0100

    Social App
    A new app for social was created so that social urls and followers will not be on the user profile

--- about/admin.py
+++ about/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import Page, About, Vission, Mission, Goal, Service
+from .models import Page, About, Vision, Mission, Goal, Service
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ("title", "body",)
@@ -10,7 +10,7 @@ class PageAdmin(admin.ModelAdmin):
 class AboutAdmin(admin.ModelAdmin):
     readonly_fields = ()
 
-class VissionAdmin(admin.ModelAdmin):
+class VisionAdmin(admin.ModelAdmin):
     readonly_fields = ()
 
 class MissionAdmin(admin.ModelAdmin):
@@ -24,7 +24,7 @@ class ServiceAdmin(admin.ModelAdmin):
 
 admin.site.register(Page, PageAdmin)
 admin.site.register(About, AboutAdmin)
-admin.site.register(Vission, VissionAdmin)
+admin.site.register(Vision, VisionAdmin)
 admin.site.register(Mission, MissionAdmin)
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(Service, ServiceAdmin)
